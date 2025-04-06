c2b5e02e0db1d27bed8c1c27350d950a6ccbc1e5
Anani Serge AMOUSSOUGBO <amoussougboserge@gmail.com>
2025-04-06 15:03:33 +0100

    Enable React strict mode and set output to standalone in Next.js configuration

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  output: "standalone", // Important pour le support des fonctions backend sur Vercel ou autre
 };
 
 export default nextConfig;
